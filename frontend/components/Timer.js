8ad755cd2e37fde8a2bb2ddc436f008057332217
PatrickMac <patrickvalera50@yahoo.com>
2022-04-21 17:46:33 -0700

    add ringtone

--- frontend/components/Timer.js
+++ frontend/components/Timer.js
@@ -2,40 +2,57 @@ import { Box, Button, Stack } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { BiStopwatch } from 'react-icons/bi'
 import { IoClose } from 'react-icons/io5'
+
 const Timer = () => {
   const [timerFormOpen, setTimerFromOpen] = useState(false)
   const [time, setTime] = useState()
   const [timerPlay, setTimerPlay] = useState(false)
-  const [timer,setTimer]=useState(null)
+  const [timer, setTimer] = useState(null)
+  const [ring, setRing] = useState()
 
-  useEffect(()=>{
-    if(timerPlay&&time){
-      setTimer(setInterval(()=>{
-        setTime(state=>state-1)
-      },1000))
-    }
-    else clearInterval(timer)
-    return(()=>clearInterval(timer))
-  },[timerPlay])
-  function getMinutes(){
-    if (!time)return '00'
-    let minute=Math.floor(time/60)
-    if(minute<10)return 0+String(minute)
+  function getMinutes() {
+    if (!time) return '00'
+    let minute = Math.floor(time / 60)
+    if (minute < 10) return 0 + String(minute)
     else return String(minute)
   }
-  function getSeconds(){
-    if (!time)return '00'
-    let seconds=time%60
-    if(seconds<10)return 0+String(seconds)
+
+  function getSeconds() {
+    if (!time) return '00'
+    let seconds = time % 60
+    if (seconds < 10) return 0 + String(seconds)
     else return String(seconds)
   }
-  useEffect(()=>{
-    if(time<=0){
-      console.log(time)
-      console.log("STOP")
+
+  useEffect(() => {
+    if (timerPlay && time) {
+      setTimer(
+        setInterval(() => {
+          setTime((state) => state - 1)
+        }, 1000)
+      )
+      if (ring) {
+        ring.pause()
+        ring.currentTime = 0
+      }
+    } else clearInterval(timer)
+    return () => clearInterval(timer)
+  }, [timerPlay])
+
+  useEffect(() => {
+    if (time <= 0) {
+      setTimerPlay(false)
+      if (ring) {
+        ring.loop = true
+        ring.play()
+      }
       clearInterval(timer)
     }
-  },[time])
+  }, [time])
+
+  useEffect(() => {
+    setRing(new Audio('./ring.mp3'))
+  }, [])
   return (
     <Box
       className='radio-stations-container'
@@ -66,13 +83,12 @@ const Timer = () => {
             minHeight: '50px',
             bgcolor: 'rgba(0,0,0,.6)',
             backdropFilter: `blur(3px)`,
-            border: '1px solid rgba(255,255,255,.9)'
-            ,
+            border: '1px solid rgba(255,255,255,.9)',
           }}
           onClick={() => setTimerFromOpen((state) => !state)}
         >
           {!timerFormOpen ? <BiStopwatch /> : <IoClose />}
-          {getMinutes()+':'+getSeconds()}
+          {getMinutes() + ':' + getSeconds()}
         </Button>
       </Box>
       {/* TIME FORM SELECT HERE */}
@@ -86,16 +102,23 @@ const Timer = () => {
         }}
       >
         <Stack direction='column'>
-          {[10, 20, 30].map((time) => (
+          {[0.1, 20, 30].map((time) => (
             <Button
               sx={{ color: 'white', minWidth: 'unset' }}
-              onClick={() =>{ setTime(time*60)
-              setTimerPlay(false)}}
+              onClick={() => {
+                setTime(time * 60)
+                setTimerPlay(false)
+              }}
             >
               {time}
             </Button>
           ))}
-          <Button sx={{ color: 'white', minWidth: 'unset' }} onClick={()=>setTimerPlay(true)}>Go</Button>
+          <Button
+            sx={{ color: 'white', minWidth: 'unset' }}
+            onClick={() => setTimerPlay(true)}
+          >
+            Go
+          </Button>
         </Stack>
       </Box>
     </Box>
